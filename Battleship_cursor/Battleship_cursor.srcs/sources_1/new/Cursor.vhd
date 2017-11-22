7b780759024aa11f74195725c563a7d49b5fe4ce
Angeld404 <adelga20@calpoly.edu>
2017-11-21 23:24:39 -0800

    new cursor

--- Battleship_cursor/Battleship_cursor.srcs/sources_1/new/Cursor.vhd
+++ Battleship_cursor/Battleship_cursor.srcs/sources_1/new/Cursor.vhd
@@ -21,16 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
+use IEEE.NUMERIC_STD.ALL;
 entity Cursor is
     Port ( Ubtn : in STD_LOGIC;
            Dbtn : in STD_LOGIC;
