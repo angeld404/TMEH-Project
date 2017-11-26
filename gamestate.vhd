e1027e667bcfde9374e83a88ecfc69e5dfd8cd45
Angeld404 <adelga20@calpoly.edu>
2017-11-26 06:42:08 -0800

    Revert "Revert "Bunch O' Changes""
    This reverts commit 0d2b613908b369a4d396f2d050ffbb13e21cee55.

--- gamestate.vhd
+++ gamestate.vhd
@@ -1,8 +1,20 @@
 process (sw1, sw2, sw3)
+variable flag : bit := '0';
+
 begin
     GameState(0) <= sw1;
     GameState(1) <= sw2;
     GameState(2) <= sw3;
 
 
--- This needs work. I need it so that when a new state is triggered, the column and row variables are reset
+    -- Once in P2 setup phase, for the first time, reset the Col/Row of cursor
+    if (GameState = "011" and flag = '0' ) then
+        OCursorCol <= "000";
+        OCursorRow <= '0';
+        flag <= '1';
+    end if;
+
+    if (GameState = "111") then
+    -- some code that fully resets game
+    end if;
+end process;
