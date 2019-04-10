df7c68d51fffd793ea5e88bb5a59d448cfa92c7b
JonathanLau2018 <JonathanLau2018@users.noreply.github.com>
2019-04-09 19:24:49 -0700

    dfasfdasf

---
+++ CPE 329/Assignments/A2/Debug/delay.d
@@ -0,0 +1,37 @@
+# FIXED
+
+delay.obj: ../delay.c
+delay.obj: C:/Users/jonat/OneDrive/Documents/ccs/ccs_base/arm/include/msp.h
+delay.obj: C:/Users/jonat/OneDrive/Documents/ccs/ccs_base/arm/include/msp432p401r.h
+delay.obj: C:/Users/jonat/OneDrive/Documents/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdint.h
+delay.obj: C:/Users/jonat/OneDrive/Documents/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/_stdint40.h
+delay.obj: C:/Users/jonat/OneDrive/Documents/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/stdint.h
+delay.obj: C:/Users/jonat/OneDrive/Documents/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/cdefs.h
+delay.obj: C:/Users/jonat/OneDrive/Documents/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_types.h
+delay.obj: C:/Users/jonat/OneDrive/Documents/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_types.h
+delay.obj: C:/Users/jonat/OneDrive/Documents/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_stdint.h
+delay.obj: C:/Users/jonat/OneDrive/Documents/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_stdint.h
+delay.obj: C:/Users/jonat/OneDrive/Documents/ccs/ccs_base/arm/include/msp_compatibility.h
+delay.obj: C:/Users/jonat/OneDrive/Documents/ccs/ccs_base/arm/include/msp432p401r_classic.h
+delay.obj: C:/Users/jonat/OneDrive/Documents/ccs/ccs_base/arm/include/CMSIS/core_cm4.h
+delay.obj: C:/Users/jonat/OneDrive/Documents/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+delay.obj: C:/Users/jonat/OneDrive/Documents/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+delay.obj: C:/Users/jonat/OneDrive/Documents/ccs/ccs_base/arm/include/system_msp432p401r.h
+
+../delay.c: 
+C:/Users/jonat/OneDrive/Documents/ccs/ccs_base/arm/include/msp.h: 
+C:/Users/jonat/OneDrive/Documents/ccs/ccs_base/arm/include/msp432p401r.h: 
+C:/Users/jonat/OneDrive/Documents/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdint.h: 
+C:/Users/jonat/OneDrive/Documents/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/_stdint40.h: 
+C:/Users/jonat/OneDrive/Documents/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/stdint.h: 
+C:/Users/jonat/OneDrive/Documents/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/cdefs.h: 
+C:/Users/jonat/OneDrive/Documents/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_types.h: 
+C:/Users/jonat/OneDrive/Documents/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_types.h: 
+C:/Users/jonat/OneDrive/Documents/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_stdint.h: 
+C:/Users/jonat/OneDrive/Documents/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_stdint.h: 
+C:/Users/jonat/OneDrive/Documents/ccs/ccs_base/arm/include/msp_compatibility.h: 
+C:/Users/jonat/OneDrive/Documents/ccs/ccs_base/arm/include/msp432p401r_classic.h: 
+C:/Users/jonat/OneDrive/Documents/ccs/ccs_base/arm/include/CMSIS/core_cm4.h: 
+C:/Users/jonat/OneDrive/Documents/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
+C:/Users/jonat/OneDrive/Documents/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+C:/Users/jonat/OneDrive/Documents/ccs/ccs_base/arm/include/system_msp432p401r.h: 
---
+++ CPE 329/Assignments/A2/delay.c
@@ -0,0 +1,19 @@
+#include "msp.h"
+
+
+/**
+ * Jonathan Lau, Angel Delgado
+ * CPE 329-11
+ * Spring 2019
+ * Assignment A2
+ */
+
+void delay_us(int u) {
+    int i, n;
+
+    for(i = u; i > 0; i--) {
+        for(n = 30; n > 0; n--) {
+
+        }
+    }
+}
