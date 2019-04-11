f84703ca12fcf2760ad9db16cd7a70260d43af0a
JonathanLau2018 <JonathanLau2018@users.noreply.github.com>
2019-04-11 00:04:06 -0700

    LCD files

---
+++ CPE 329/Assignment 2/lcd.h
@@ -0,0 +1,29 @@
+/*
+ * lcd.h
+ *
+ *  Created on: Apr 10, 2019
+ *  Author: Jonathan Lau, Angel Delgado
+ */
+
+#ifndef LCD_H_
+#define LCD_H_
+
+#define
+
+void Clear_LCD() {
+
+}
+
+void Home_LCD() {
+
+}
+
+void Write_char_LCD() {
+
+}
+
+void Write_string_LCD() {
+
+}
+
+#endif /* LCD_H_ */
